bab9a923a068449c4b90c0172b2f37655ea7d4aa
Avaadora <manonber31@gmail.com>
2020-09-18 22:43:33 +0200

    Liste triée et ordonnée

--- devoir1.java
+++ devoir1.java
@@ -1,63 +1,25 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
+import java.util.List;
+import java.util.HashSet;
 
 class App {
   public static void main(final String[] args) {
+    
+    ArrayList<String> fruits = new ArrayList<String>();
+      fruits.add("Orange");
+      fruits.add("Apple");
+      fruits.add("Banana");
+      fruits.add("Pineapple");
+      fruits.add("Banana");
+      fruits.add("Pineapple");
+      Collections.sort(fruits); //Range par ordre alphabétique.
 
-    String[] words = {
-        "coco",
-        "pedro",
-        "pedro",
-        "pedro",
-        "pedro",
-        "jaco",
-        "coco",
-        "jaco",
-      };
-      String[] newList = new String[words.length];
-      for (int i = 0; i < words.length; i++) {
-        int index = -1;
-        String word = words[i];
-        for (int j = 0; j < newList.length; j++) {
-          if (newList[j] == null || newList[j].equals(word)) {
-            index = j;
-          }
-        }
-        if (index >= 0) {
-          newList[index] = word;
-        }
-      }
+      Set<String> triFruit = new HashSet<String>(fruits);
 
-      for (int k =0 ; k < newList.length; k++){
-          if (newList[k] == null){
-          }
-          else{
-            System.out.println(Arrays.toString(newList)); // --> SANS DOUBLONS !
-            // System.out.print(newList[k] + ", ");
-      }
+      ArrayList<String> fruit = new ArrayList<String>(triFruit);
+
+      System.out.println(fruit);      
   }
-  
-    // final String[] words = { "Livre", "Avion", "Zébre", "Pierre", "Feuille", "Maison", "Riche", "Sel", };
-
-    // final String[] listeTrie = new String[words.length];
-
-    // for (int i = 0; i < words.length; i++) {
-    //   int indexAtWord = -1;
-    //   final String word = words[i];
-
-    //   for(int j = 0; j < listeTrie.length; j++) {
-    //     if(listeTrie[j] == null || listeTrie[j].equals(word)) {
-    //       indexAtWord = j;
-    //     }
-    //   }
-
-    //   if(indexAtWord >= 0) {
-    //     listeTrie[indexAtWord] = word;
-
-    //   }
-    // }
-    // Arrays.sort(listeTrie);
-
-    // System.out.println(Arrays.toString(listeTrie)); 
-  }
-
 }
